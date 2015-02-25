bb1b7b6cc20639d2e37ff1abb67bd59f1880f7ab
Philippe Coval <philippe.coval@open.eurogiciel.org>
2015-02-25 11:11:50 +0100

    WIP: update jqm
    Change-Id: Ia7570eab681e91bd4765fbb1af925aba6b4e529c
    Signed-off-by: Philippe Coval <philippe.coval@open.eurogiciel.org>

--- js/main.js
+++ js/main.js
@@ -1,30 +1,45 @@
-/*
+var backEventListener = null;
 
-Copyright (c) 2013 Mapo developpers and contributors <mapo.tizen@gmail.com>
+var unregister = function() {
+    if ( backEventListener !== null ) {
+        document.removeEventListener( 'tizenhwkey', backEventListener );
+        backEventListener = null;
+        window.tizen.application.getCurrentApplication().exit();
+    }
+}
 
-This file is part of Mapo.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
-
-var initializer = function () {
-    // Initialization job
-	init();
-	swipePage();
-	// End of initialization
+//Initialize function
+var init = function () {
+    // register once
+    if ( backEventListener !== null ) {
+        return;
+    }
+    
+    // TODO:: Do your initialization job
     console.log("init() called");
+    
+    var backEvent = function(e) {
+        if ( e.keyName == "back" ) {
+            try {
+                if ( $.mobile.urlHistory.activeIndex <= 0 ) {
+                    // if first page, terminate app
+                    unregister();
+                } else {
+                    // move previous page
+                    $.mobile.urlHistory.activeIndex -= 1;
+                    $.mobile.urlHistory.clearForward();
+                    window.history.back();
+                }
+            } catch( ex ) {
+                unregister();
+            }
+        }
+    }
+    
+    // add eventListener for tizenhwkey (Back Button)
+    document.addEventListener( 'tizenhwkey', backEvent );
+    backEventListener = backEvent;
 };
 
-window.onload = initializer;
+$(document).bind( 'pageinit', init );
+$(document).unload( unregister );
